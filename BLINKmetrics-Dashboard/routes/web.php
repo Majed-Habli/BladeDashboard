346b7d980e42882687c14e91da0d5023cdfb1f45
User <majedhabli2020@gmail.com>
2023-10-09 11:39:59 +0300

    Removed commented lines.

--- BLINKmetrics-Dashboard/routes/web.php
+++ BLINKmetrics-Dashboard/routes/web.php
@@ -4,25 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\APIController;
 use App\Http\Controllers\MapController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// });
-
-// Route::get('/map', [MapController::class, 'index']);
 Route::get('/dashboard', [APIController::class, 'index']);
-// Route::get('/dashboard', [APIController::class, 'charType']);
